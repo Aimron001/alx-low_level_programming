8757393a436e6bfda68c8bbdf438a1588787d943
Ronald <ronaldmutua617@gmail.com>
2023-02-16 11:31:32 -0800

    Hello, printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /*
- * main 
- * return - 0
+ * main
+ * * return - 0
  */
-int main(void){
+int main(void)
+}
 	puts("\""Programming is like building a multilingual puzzle");
 	return (0);
 }
