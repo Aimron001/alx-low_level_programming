bd1e8cce7a301170f8f88a74a7f863bc732de799
Ronald <ronaldmutua617@gmail.com>
2023-02-18 12:58:57 -0800

    When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the alphabet in lowercase
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'a')
+	{
+		if ((ch != 'e') && (ch != 'q'))
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
