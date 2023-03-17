aedd32f8f49718650857801bda42953a9e4f21b2
Ronald <ronaldmutua617@gmail.com>
2023-03-17 10:52:06 -0700

    task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -27,7 +27,7 @@ int is_digit(char *s)
 int _strlen(char *s)
 {
 	int i = 0;
-	
+
 	while (s[i] != '\0')
 	{
 		i++;
@@ -51,8 +51,9 @@ void errors(void)
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
-	
+
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
@@ -91,4 +92,3 @@ int main(int argc, char *argv[])
 	free(result);
 	return (0);
 }
-
