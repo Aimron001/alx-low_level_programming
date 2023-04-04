bc6d6e990f8377069309b76f6f7baf41eea9f8b1
Aimron001 <ronaldmutua617@gmail.com>
2023-04-04 09:49:03 -0700

    task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns number of elements in a linked lists
+ * @h: linked list to traverse
+ *
+ * Return: number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
+
