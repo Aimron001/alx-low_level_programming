b2db496af658f53edea4499f6cc9f5ef8b9b3a83
Aimron001 <ronaldmutua617@gmail.com>
2023-04-04 12:17:54 -0700

    fixes

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -8,7 +8,7 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *tmp = *head;
-	listint_t currIndex = NULL;
+	listint_t *currIndex = NULL;
 	unsigned int i = 0;
 
 	if (*head == NULL)
