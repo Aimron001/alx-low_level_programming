17c85bf85928411395f87a49335c5e66e95fa20e
Ronald Mutua <ronaldmutua617@gmail.com>
2023-06-19 12:32:42 -0700

    task 1

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc -Wall -wextra -werror -pedantic -c -fpic *.c
+gcc -Wall -Wextra -Werror -pedantic -c -fpic *.c
 gcc -shared -o liball.so *.o
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
